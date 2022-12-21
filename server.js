08b9b4202b05b21b88bc804710b81bb7c9b4f29b
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-12-21 12:02:51 -0500

    deleted static folder and placed scripts in public

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ connectDB();
 app.set("view engine", "ejs");
 
 //Static Folder
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '../public'));
 
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
